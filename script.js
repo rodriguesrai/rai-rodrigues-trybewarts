ea3b691e761dc84dbebf03728fb5e34c868ae238
rodriguesrai <rairodrigues_sf@hotmail.com>
2023-04-26 16:09:09 -0300

    requisito 1, 2 e 3 completos

---
+++ script.js
@@ -0,0 +1,13 @@
+const botaoLogin = document.querySelector('#botao-login');
+const alertaLogin = () => {
+    
+    const pegaEmail = document.querySelector('#email');
+    const pegaSenha = document.querySelector('#senha');
+    if (pegaEmail.value === 'tryber@teste.com' && pegaSenha.value === '123456') {
+        alert('Olá, Tryber!');
+    } else {
+        alert('Email ou senha inválidos.');
+    }
+}
+
+botaoLogin.addEventListener('click', alertaLogin);
