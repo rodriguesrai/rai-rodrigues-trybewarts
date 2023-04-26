30a3591bd8f2ad0f313b8cc765e1454c90c00384
rodriguesrai <rairodrigues_sf@hotmail.com>
2023-04-26 20:57:04 -0300

    requisito 20 incompleto

--- script.js
+++ script.js
@@ -28,5 +28,26 @@ const counter = document.querySelector('#counter');
 const updateCounter = () => {
   counter.innerText = 500 - textArea.value.length;
   console.log(textArea.value.length);
-}
+};
 textArea.addEventListener('keyup', updateCounter);
+
+const formData = document.createElement('div');
+formData.setAttribute('id', 'form-data');
+
+
+
+botaoEnviar.addEventListener('click', (event) => {
+  const nome = document.getElementById('input-name').value;
+  const sobrenome = document.getElementById('input-lastname').value;
+  const email = document.getElementById('input-email').value;
+  const casa = document.getElementById('house').value;
+  const familia = document.querySelector('input[name="family"]:checked').value;
+  const conteudos = document.querySelectorAll('.subject');
+  for () {
+    
+  }
+  event.preventDefault();
+  console.log(conteudos);
+   
+});
+
