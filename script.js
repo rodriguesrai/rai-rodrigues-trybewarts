b816235d0e2f5f37940dedd1801e2b6266b873dc
joaoluizc <joaocoelho.tdm@gmail.com>
2023-04-26 21:43:28 -0300

    primeira tentativa requisito 21

--- script.js
+++ script.js
@@ -27,27 +27,50 @@ const textArea = document.querySelectorAll('.textarea')[1];
 const counter = document.querySelector('#counter');
 const updateCounter = () => {
   counter.innerText = 500 - textArea.value.length;
-  console.log(textArea.value.length);
 };
 textArea.addEventListener('keyup', updateCounter);
 
-const formData = document.createElement('div');
+const main = document.querySelector('.main');
+const formData = document.createElement('p');
 formData.setAttribute('id', 'form-data');
 
-
-
 botaoEnviar.addEventListener('click', (event) => {
+  event.preventDefault();
+
   const nome = document.getElementById('input-name').value;
   const sobrenome = document.getElementById('input-lastname').value;
   const email = document.getElementById('input-email').value;
   const casa = document.getElementById('house').value;
   const familia = document.querySelector('input[name="family"]:checked').value;
   const conteudos = document.querySelectorAll('.subject');
-  for () {
-    
+  const avaliacao = document.querySelector('input[name="rate"]:checked').value;
+  const observacoes = document.getElementById('textarea').value;
+
+  // Percorre NodeList de conteudos selecionados
+  let conteudosStr = 'Matérias:';
+  for (let i = 0; i < conteudos.length; i += 1) {
+    if (conteudos[i].checked === true) {
+    if (i === 0) {
+      conteudosStr = conteudosStr + ' ' + conteudos[i].value;
+    } else {
+    conteudosStr = conteudosStr + ', ' + conteudos[i].value;
+    }
   }
-  event.preventDefault();
-  console.log(conteudos);
-   
+
+  const strFinal = `Nome: ${nome} ${sobrenome} <br>
+  Email: ${email} <br>
+  Casa: ${casa} <br>
+  Família: ${familia} <br>
+  Matérias: ${conteudosStr} <br>
+  Avaliação: ${avaliacao} <br>
+  Observações: ${observacoes} <br>
+  `;
+
+  formData.innerHTML = strFinal;
+  main.firstChild.replaceWith(formData);
+  main.removeChild(document.querySelector('#evaluation-form'));
+  // main.prependChild(formData);
+
+  }
 });
 
